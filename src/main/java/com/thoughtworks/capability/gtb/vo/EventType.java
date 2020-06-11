0bee5d2b7882fd3b62acc2df47e6e8c498a45f9b
teng.li2 <teng.li2@thoughworks.com>
2020-06-11 11:42:45 +0800

    [teng/li][2020-6-11] serialize homework

--- src/main/java/com/thoughtworks/capability/gtb/vo/EventType.java
+++ src/main/java/com/thoughtworks/capability/gtb/vo/EventType.java
@@ -1,10 +1,13 @@
 package com.thoughtworks.capability.gtb.vo;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 /**
  * @author itutry
  * @create 2020-05-21_16:26
  */
 public enum EventType {
+
   UPLOAD("U"), DOWNLOAD("D");
 
   private String code;
@@ -12,4 +15,10 @@ public enum EventType {
   EventType(String code) {
     this.code = code;
   }
+
+  @JsonValue
+  public String getCode(){
+    return code;
+  }
+
 }
--- src/main/java/com/thoughtworks/capability/gtb/vo/EventVo.java
+++ src/main/java/com/thoughtworks/capability/gtb/vo/EventVo.java
@@ -1,6 +1,26 @@
 package com.thoughtworks.capability.gtb.vo;
 
+import java.io.IOException;
+import java.time.Instant;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonUnwrapped;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+import com.fasterxml.jackson.databind.node.IntNode;
+import com.fasterxml.jackson.databind.node.LongNode;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,11 +32,60 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class EventVo {
 
   private String id;
   private String name;
   private EventType type;
+  @JsonSerialize(using = LocalDateTimeSerializer.class)
+  @JsonDeserialize(using = LocalDateTimeDeserialize.class)
   private LocalDateTime time;
+  @JsonUnwrapped
   private UserVo user;
+
+  /*
+  * 自定义序列化器 北京时间 毫秒级
+  * */
+  private static class LocalDateTimeSerializer extends StdSerializer<LocalDateTime>{
+
+    public LocalDateTimeSerializer() {
+      super(LocalDateTime.class);
+    }
+
+    @Override
+    public void serialize(LocalDateTime value, JsonGenerator gen, SerializerProvider provider) throws IOException {
+
+        gen.writeNumber(value.toInstant(ZoneOffset.of("+8")).toEpochMilli());
+
+    }
+  }
+
+  /*
+  * 自定义反序列化器 本地时区
+  *
+  * */
+
+  private static class LocalDateTimeDeserialize extends StdDeserializer<LocalDateTime>{
+
+    public LocalDateTimeDeserialize() {
+      super(LocalDateTime.class);
+    }
+
+    @Override
+    public LocalDateTime deserialize(JsonParser p, DeserializationContext ctxt) throws IOException, JsonProcessingException {
+
+      JsonNode node = p.getCodec().readTree(p);
+
+      long instantValue= node.asLong();//取得instant值
+
+      ZoneId zone = ZoneId.systemDefault();
+
+      Instant instant= Instant.ofEpochMilli(instantValue);
+
+      LocalDateTime localDateTime = LocalDateTime.ofInstant(instant, zone);//转换
+
+      return localDateTime;
+    }
+  }
 }
